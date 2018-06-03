885e8031623bc0b03325b47ee6ac6bb72058fd86
Atul <dharwal512@gmail.com>
2018-06-03 09:46:55 +0530

    GIT Jenkins 1st commit

---
+++ src/main/java/com/jenkins/demo/JenkinsDemo.java
@@ -0,0 +1,17 @@
+package com.jenkins.demo;
+
+
+class JenkinsDemo {
+
+    int var;
+    String myStr;
+
+    JenkinsDemo(){
+    }
+
+ 
+    public void sayhelloJenkins(){
+        System.out.println(" Hello jenkins !!!!");
+    }
+
+}
